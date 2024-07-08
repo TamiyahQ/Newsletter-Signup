ed66e012beb2c1614fa74b8a67e0d94d2db0c827
TamiyahQ <165490293+TamiyahQ@users.noreply.github.com>
2024-07-08 12:32:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var subscribe = document.getElementById('submitbtn');
+subscribe.addEventListener('click', runEvent);
+
+
+function runEvent(e) {
+
+    e.preventDefault();
+    window.location.href='index1.html';
+}
+
+
+
+
